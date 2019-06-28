fb397a0bbc70aa48a5cf45cc18fe4673540fb6c1
BinSin <binyong318@gmail.com>
2019-06-28 19:19:39 +0900

    using annotation

--- helloDB/src/main/java/com/binsin/mall/OfferDAO.java
+++ helloDB/src/main/java/com/binsin/mall/OfferDAO.java
@@ -4,7 +4,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component("offerDAO")
 public class OfferDAO {
 	private JdbcTemplate jdbcTemplate;
 	
