088f7b65556b89f7c1fd18fc53d2caa2f6a38673
BinSin <binyong318@gmail.com>
2019-06-28 21:42:08 +0900

    DB CRUD

--- helloDB/src/main/java/com/binsin/mall/MainApp.java
+++ helloDB/src/main/java/com/binsin/mall/MainApp.java
@@ -1,5 +1,7 @@
 package com.binsin.mall;
 
+import java.util.List;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
@@ -8,6 +10,42 @@ public class MainApp {
 		
 		OfferDAO offerDAO = (OfferDAO)context.getBean("offerDAO");
 		System.out.println("The record count is " + offerDAO.getRowCount());
+		
+		List<Offer> offerList = offerDAO.getOffers();
+		
+		for(Offer offer : offerList) {
+			System.out.println(offer);
+		}
+		
+		// insert
+		Offer offer = new Offer();
+		offer.setId(5);
+		offer.setName("binsin");
+		offer.setEmail("binsin@binsin.com");
+		offer.setText("hello~~!!!");
+		if(offerDAO.insert(offer)) {
+			System.out.println("Object is inserted successfully");
+		} else {
+			System.out.println("Object insert failed");
+		}
+		
+		// update
+		offer = offerDAO.getOffer("binsin");
+		offer.setName("binsinKing");
+		if(offerDAO.update(offer)) {
+			System.out.println("Object is updated successfully");
+		} else {
+			System.out.println("Object update failed");
+		}
+		System.out.println(offer);
+			
+		// delete
+		offer = offerDAO.getOffer("binsinKing");
+		if(offerDAO.delete(offer.getId())) {
+			System.out.println("Object is deleted successfully");
+		} else {
+			System.out.println("Object delete failed");
+		}
 		
 		context.close();
 	}
