35bff645336cb98f80bc5ab3b961736bdbe7b607
BinSin <binyong318@gmail.com>
2019-07-04 17:22:16 +0900

    use logback

--- helloSpringMVC/src/main/java/com/binsin/controller/HomeController.java
+++ helloSpringMVC/src/main/java/com/binsin/controller/HomeController.java
@@ -1,8 +1,8 @@
 package com.binsin.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,27 +11,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	// com.binsin 에 있던 로그 내용을 상속 받는다. -> daily, minutes, console 모두 상속 받은 상태
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class); // com.binsin.controller.HomeController
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(HttpServletRequest request, Locale locale, Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		// level value="DEBUG" 이므로 debug 레벨보다 높은 것들만 출력된다.
+		// logger.trace("trace level: Welcome home! The client locale is {}", locale);
+		// logger.debug("debug level: Welcome home! The client locale is {}", locale);
+		logger.info("info level: Welcome home! The client locale is {}", locale);
+		// logger.warn("warn level: Welcome home! The client locale is {}", locale);
+		// logger.error("error level: Welcome home! The client locale is {}", locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		String url = request.getRequestURL().toString();
+		String clientIPaddress = request.getRemoteAddr();
+		logger.info("Request URL: " + url);
+		logger.info("Client IP: " + clientIPaddress);
 		
 		return "home";
 	}
