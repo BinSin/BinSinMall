fdb94d636e3ffa94daff5c80b813ee6b73ee51a9
BinSin <binyong318@gmail.com>
2019-07-16 20:08:19 +0900

    one to one

---
+++ testHibernate/src/main/java/testHibernate/Person.java
@@ -0,0 +1,29 @@
+package testHibernate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;	
+
+	@OneToOne(mappedBy="person", cascade=CascadeType.ALL)
+	private License license;
+	
+}
--- testHibernate/src/main/java/testHibernate/testMain.java
+++ testHibernate/src/main/java/testHibernate/testMain.java
@@ -1,5 +1,7 @@
 package testHibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -47,6 +49,26 @@ public class testMain {
 		product3.setCategory(category2);
 		category2.getProducts().add(product3);
 		
+		Person person1 = new Person();
+		person1.setFirstName("sin");
+		person1.setLastName("bin");
+		
+		License license1 = new License();
+		license1.setLicenseNumber("1234");
+		license1.setIssueDate(new Date());
+		license1.setPerson(person1);
+		person1.setLicense(license1);
+		
+		Person person2 = new Person();
+		person2.setFirstName("sing");
+		person2.setLastName("bing");
+		
+		License license2 = new License();
+		license2.setLicenseNumber("5678");
+		license2.setIssueDate(new Date());
+		license2.setPerson(person2);
+		person2.setLicense(license2);
+		
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
 	
@@ -57,8 +79,14 @@ public class testMain {
 		session.save(product3);
 		*/
 
-		session.save(category1);
-		session.save(category2);
+		//session.save(category1);
+		//session.save(category2);
+		
+		//session.save(license1);
+		//session.save(license2);
+		
+		session.save(person1);
+		session.save(person2);
 		
 		// session.delete(product1); // product1 -> category1 -> product2
 		// session.delete(category1);
