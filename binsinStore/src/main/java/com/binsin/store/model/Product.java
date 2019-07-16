cd408a127c5da9e9b9e7d0fde259c0880cb343d1
BinSin <binyong318@gmail.com>
2019-07-16 20:27:32 +0900

    back

--- binsinStore/src/main/java/com/binsin/store/model/Product.java
+++ binsinStore/src/main/java/com/binsin/store/model/Product.java
@@ -1,11 +1,5 @@
 package com.binsin.store.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
@@ -16,13 +10,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@Entity
-@Table(name="product")
 public class Product {
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY) // MySQL에서는 AUTO로 주면 TABLE로 설정이 된다. 이 경우 Sequence number를 저장하는 테이블을 만든다.
-	@Column(name="product_id")						  // IDENTITY로 설정하면 auto_increment로 설정하여 sequence number 테이블을 만들지 않아 퍼포먼스에 좋다.
 	private int id;
 	
 	@NotEmpty(message="The product name must not be null")
